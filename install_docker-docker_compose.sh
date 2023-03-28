27534b3e8efadfa73a027d3b389d7609709e5d1b
nvdatgithub <91255473+nvdatgithub@users.noreply.github.com>
2023-03-28 18:57:56 +0700

    Update install_docker-docker_compose.sh

--- install_docker-docker_compose.sh
+++ install_docker-docker_compose.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+# lấy quyền root
 sudo su -
+# update centos
 apt update -y
+# install docker từ file docker.io
 apt install docker.io -y
+# bật docker
 systemctl enable docker
+# cài docker compose
 apt-get install curl -y
 curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# cài đặt quyền cho docker compese
 chmod +x /usr/local/bin/docker-compose
